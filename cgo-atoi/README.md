a7629566f5f9496795737eb066c57d62c1230f91
Alex Sergeyev <asergeyev@dyn.com>
2014-12-18 14:21:39 -0500

    Duh... "null terminated buffer"

--- cgo-atoi/README.md
+++ cgo-atoi/README.md
@@ -1,2 +1,5 @@
 Quick test if it's worth to do anything about dealing with
 strconv.Atoi(string(buffer)) or it's pretty much as fast as it gets
+
+
+Apparetnly CGo is slower here. 
--- cgo-atoi/atoi_test.go
+++ cgo-atoi/atoi_test.go
@@ -5,10 +5,12 @@ import (
 	"testing"
 )
 
-var buf = []byte("123456")
+// note this only works on null-terminated buffer and length 6
+// since it's a very simple toy
+var buf = append([]byte("123456"), 0)
 
 func TestAtoi(t *testing.T) {
-	i1, _ := strconv.Atoi(string(buf))
+	i1, _ := strconv.Atoi(string(buf[:6]))
 	i2, _ := CAtoi(buf)
 	if i1 != i2 {
 		t.Errorf("Answer from CAtoi is different than from Go version (%d)", i2)
@@ -17,7 +19,7 @@ func TestAtoi(t *testing.T) {
 
 func BenchmarkGo(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		strconv.Atoi(string(buf))
+		strconv.Atoi(string(buf[:6]))
 	}
 }
 
