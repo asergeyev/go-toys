5b62af45d04ea057182e6607f63add249f545187
Alex Sergeyev <abc@alexsergeyev.com>
2015-05-21 15:17:43 -0400

    Updating potter files

--- potter/potter.go
+++ potter/potter.go
@@ -1,53 +1,24 @@
-// potter - small personal toy project, abstract map[[]byte]uint32 implementation...
 package potter
 
 import (
 	"bytes"
-	"sync"
 )
 
-// Pot is a map[uint32]uint32 implementation
 type Pot struct {
-	buffers [][]byte
-	hash    []uint32
-	pos     []uint64
-	ln      []byte
-	points  uint32
-	*sync.RWMutex
+	buffer [][]byte
+	refs   map[uint32][]byte
 }
 
-// BufferSize controls amount of bytes per slice of keys buffer. You can change it any time, it gets used by NewPot and by
-// Encode when new slice is necessary to add.
-var BufferSize uint32 = 5 * 1024 * 1024 // do not overflow it!
+const BUFSIZE = uint32(1024 * 1024)
 
-// NewPot reserves space for hash elements.
-func NewPot(seats uint32) *Pot {
-	spaces := int(float64(seats) / 3.0 * 4)
+func NewPot(seats int) *Pot {
 	return &Pot{
-		buffers: [][]byte{make([]byte, 0, BufferSize)},
-		hash:    make([]uint32, spaces),
-		pos:     make([]uint64, spaces),
-		ln:      make([]byte, spaces),
-		points:  seats,
-		RWMutex: &sync.RWMutex{},
+		buffer: [][]byte{make([]byte, 0, BUFSIZE)},
+		refs:   make(map[uint32][]byte, seats),
 	}
-}
-
-// Decode digs out value by given int representation.
-func (p *Pot) Decode(x uint32) []byte {
-	p.RLock()
-	defer p.RUnlock()
-	if p.pos[x] == 0 {
-		// this is not here at all
-		return nil
-	}
-	start := uint32(p.pos[x])
-	current := p.buffers[uint32(p.pos[x]>>32)-1]
-	return current[start : start+uint32(p.ln[x])]
 }
 
 func hashval(buf []byte) uint32 {
-	// basuc cdb hash
 	hash := uint32(5381)
 	for _, b := range buf {
 		hash = (hash + (hash << 5)) ^ uint32(b)
@@ -55,87 +26,35 @@ func hashval(buf []byte) uint32 {
 	return hash
 }
 
-// Encode checks if value already seen and returns it's position; if value is not seen it's added to buffer and new position returned.
+func (p *Pot) Find(buf []byte) (h uint32, _ bool) {
+	h = hashval(buf)
+	for p.refs[h] != nil {
+		if bytes.Equal(p.refs[h], buf) {
+			return h, true
+		}
+		h++
+	}
+	return h, false
+}
+
 func (p *Pot) Encode(buf []byte) uint32 {
-	if len(buf) > 255 {
-		panic("long buffer could not be encoded")
+	h, ok := p.Find(buf)
+	if ok {
+		return h
 	}
-	hash := hashval(buf)
+	current := len(p.buffer) - 1
+	start := len(p.buffer[current])
+	if start+len(buf) > cap(p.buffer[current]) {
+		current++
+		start = 0
+		p.buffer = append(p.buffer, make([]byte, 0, BUFSIZE))
+	}
+	p.buffer[current] = append(p.buffer[current], buf...)
+	p.refs[h] = p.buffer[current][start : start+len(buf)]
+	return h
 
-	p.RLock()
-	for i := 0; i < len(p.ln); i++ {
-		x := (hash + uint32(i)) % uint32(len(p.ln))
-		if p.pos[x] == 0 {
-			p.RUnlock()
-			p.Lock()
-			if p.points == 0 {
-				p.resize()
-			}
-			current := p.buffers[len(p.buffers)-1]
-			if len(current)+len(buf) > cap(current) {
-				current = make([]byte, 0, BufferSize)
-				p.buffers = append(p.buffers, current)
-			}
-			p.ln[x] = byte(len(buf))
-			p.hash[x] = hash
-			p.pos[x] = uint64(len(p.buffers))<<32 + uint64(len(current))
-			current = append(current, buf...)
-			p.buffers[len(p.buffers)-1] = current
-			p.points -= 1
-			p.Unlock()
-			return uint32(x)
-		} else if p.hash[x] == hash && byte(len(buf)) == p.ln[x] {
-			start := uint32(p.pos[x])
-			current := p.buffers[uint32(p.pos[x]>>32)-1]
-			if start+uint32(len(buf)) <= uint32(len(current)) {
-				val := current[start : start+uint32(len(buf))]
-				if bytes.Equal(val, buf) {
-					p.RUnlock()
-					return x
-				}
-			}
-		}
-	}
-	panic("pot is full")
 }
 
-// SendAllKeys streams all taken hashtab positions to channel
-func (p *Pot) SendAllKeys(x chan int) {
-	p.RLock()
-	defer p.RUnlock()
-	for slot, pos := range p.pos {
-		if pos > 0 {
-			x <- slot
-		}
-	}
-	close(x)
-}
-
-func (p *Pot) resize() {
-	// no locking here
-
-	// generate new arrays for ((currentsize+1) * 1.6)
-	addsize := float64(len(p.ln)+1) * 0.6
-	p.points += uint32(addsize*.75 + .5)
-
-	newlen := uint32(len(p.ln) + int(addsize+.5))
-	newln, newhash, newpos := make([]byte, newlen), make([]uint32, newlen), make([]uint64, newlen)
-
-HASH:
-	// replay the hashes and insert new values
-	for x, hash := range p.hash {
-		for i := 0; i < len(p.ln); i++ {
-			newx := (hash + uint32(i)) % newlen
-			if newpos[newx] == 0 {
-				newpos[newx] = p.pos[x]
-				newln[newx] = p.ln[x]
-				newhash[newx] = hash
-				continue HASH
-			}
-		}
-		panic("could not resize, all spots are taken!")
-	}
-
-	// swap old arrays to new ones
-	p.ln, p.hash, p.pos = newln, newhash, newpos
+func (p *Pot) Decode(n uint32) []byte {
+	return p.refs[n]
 }
--- potter/potter_test.go
+++ potter/potter_test.go
@@ -1,89 +1,52 @@
 package potter
 
 import (
-	"bytes"
 	"fmt"
+	"math/rand"
 	"testing"
 )
 
-func TestPotting(t *testing.T) {
-	x := NewPot(10)
-	if len(x.ln) != 13 {
-		t.Error("Wrong initial size, expected 13 got ", len(x.ln))
-	}
-
-	seen := map[uint32]bool{}
-	for i := byte(0); i < 10; i++ {
-		num := x.Encode([]byte{'a' + i})
-		if seen[num] {
-			t.Errorf("Same number as seen before for new value!")
-		} else {
-			seen[num] = true
+func TestNewPot(t *testing.T) {
+	x := NewPot(10000)
+	for i := 0; i < 65535; i++ {
+		core := []byte{byte(i), byte(i >> 8), byte(rand.Int31()), byte(rand.Int31())}
+		pt := x.Encode(core)
+		ret := x.Decode(pt)
+		if len(ret) != 4 || ret[0] != core[0] || ret[1] != core[1] {
+			t.Errorf("Invald potting %v != %v", ret, core)
 		}
-		if x.points != uint32(10-i-1) {
-			t.Errorf("Points counter is wrong should've had %d but got %d", 10-i-1, x.points)
-		}
-		if len(x.ln) != 13 {
-			t.Error("Resize triggered when not expected!", len(x.ln), "slot", i)
-		}
-
-	}
-
-	for i := byte(0); i < 10; i++ {
-		bt := []byte{'a' + i}
-		pt := x.Encode(bt)
-		if !seen[pt] {
-			t.Errorf("New number for previously seen value!")
-		}
-		if got := x.Decode(pt); !bytes.Equal(bt, got) {
-			t.Errorf("Expected %s and got %s", pt, got)
-		}
-		if x.points != 0 {
-			t.Errorf("Points counter is wrong should've had 0 but got %d", x.points)
-		}
-		if len(x.ln) != 13 {
-			t.Error("Resize triggered when not expected!", len(x.ln), "slot", i)
-		}
-	}
-
-	x.Encode([]byte("ok"))
-	// should've resized
-	if len(x.ln) != 21 {
-		t.Error("Wrong resized size, expected 21 got ", len(x.ln))
-	}
-	if x.points != 6-1 { // added 6 and 1 was taken
-		t.Errorf("Did not get enough points, expected %d got %d", 5, x.points)
 	}
 }
 
-// benchmarks on 100K value population
-
-func BenchmarkPottingNew(b *testing.B) {
-	x := NewPot(99999) // one resize after things done
-	for i := 0; i < 100000; i++ {
+func BenchmarkPotting(b *testing.B) {
+	x := NewPot(b.N)
+	for i := 0; i < b.N; i++ {
 		x.Encode([]byte(fmt.Sprint(i)))
-	}
-	for i := 0; i < b.N; i++ { // treating i as uint32
-		x.Encode([]byte(fmt.Sprint("abcde", i)))
 	}
 }
 
-func BenchmarkPottingOld(b *testing.B) {
-	x := NewPot(100000) // one resize after things done
-	for i := 0; i < 100000; i++ {
+func BenchmarkPottingLookupUnknown(b *testing.B) {
+	test := make([][]byte, b.N)
+	x := NewPot(b.N)
+	for i := 0; i < b.N; i++ {
+		test[i] = []byte(fmt.Sprint("abcdef", i))
 		x.Encode([]byte(fmt.Sprint(i)))
 	}
+	b.ResetTimer()
 	for j := 0; j < b.N; j++ {
-		i := j % 100000
-		x.Encode([]byte(fmt.Sprint(i)))
-
+		x.Encode(test[j])
 	}
 }
 
-// func BenchmarkPottingOld(b *testing.B) {
-
-// }
-
-// func BenchmarkPottingRandom(b *testing.B) {
-
-// }
+func BenchmarkPottingLookupKnown(b *testing.B) {
+	test := make([][]byte, b.N)
+	x := NewPot(b.N)
+	for i := 0; i < b.N; i++ {
+		test[i] = []byte(fmt.Sprint(i))
+		x.Encode([]byte(fmt.Sprint(i)))
+	}
+	b.ResetTimer()
+	for j := 0; j < b.N; j++ {
+		x.Encode(test[j])
+	}
+}
