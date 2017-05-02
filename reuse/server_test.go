976f0f7f63b2c8c58ac4facf52a82301009a1e67
Alex Sergeyev <abc@alexsergeyev.com>
2017-05-01 23:02:10 -0400

    Add Listen error checking

--- reuse/server_test.go
+++ reuse/server_test.go
@@ -1,12 +1,16 @@
 package reuse
 
-import "testing"
+import (
+	"net"
+
+	"testing"
+)
 
 func TestServer(t *testing.T) {
 	var err error
 
 	port := 5555
-	workers := make([]*Service, 3)
+	workers := make([]*Service, 5)
 
 	for i := 0; i < len(workers); i++ {
 		workers[i], err = NewService(port)
@@ -14,6 +18,13 @@ func TestServer(t *testing.T) {
 			t.Fatalf("Failed to create clone #%d %s", i, err)
 		}
 	}
-	
-	// success
+
+	// success, now prove it's not possible to Listen on same..
+
+	addr := workers[len(workers)-1].l.LocalAddr() // this one is definitely cloned
+
+	_, err = net.ListenUDP("udp", addr.(*net.UDPAddr))
+	if err == nil {
+		t.Fatal("Expectations failed, Listen worked")
+	}
 }
