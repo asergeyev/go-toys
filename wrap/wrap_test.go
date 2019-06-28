ff715815e3cd41d5130b75789765021b2a56d454
Alex Sergeyev <asergeyev@fastly.com>
2019-06-28 09:55:07 -0700

    Add comments

--- wrap/wrap_test.go
+++ wrap/wrap_test.go
@@ -7,7 +7,7 @@ import (
 
 func BenchmarkWrap(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		err := errors.Wrap(nil, "test")
+		err := errors.Wrap(nil, "test") // just make sure there is some work to set err 
 		if ts := errors.Wrap(err, "test"); ts != nil {
 			panic("not here")
 		}
@@ -15,7 +15,7 @@ func BenchmarkWrap(b *testing.B) {
 }
 func BenchmarkNoWrap(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		err := errors.Wrap(nil, "test")
+		err := errors.Wrap(nil, "test") // just make sure there is some work to set err 
 		if err != nil {
 			panic("not here")
 		}
