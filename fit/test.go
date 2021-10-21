14dd58da062376d706cf31a58c0b5c9c2f1bd83a
Alex Sergeyev <asergeyev@fastly.com>
2021-10-21 13:33:51 -0400

    I don't do much perf code recently so it's a test for future tinkering

---
+++ fit/test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/tormoder/fit"
+)
+
+func main() {
+	// Read our FIT test file data
+	testData, err := ioutil.ReadFile("example.fit")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Decode the FIT file data
+	fit, err := fit.Decode(bytes.NewReader(testData))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Get the actual activity
+	activity, err := fit.Activity()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the latitude and longitude of the first Record message
+	prev := uint32(0)
+	total := uint32(0)
+	for _, record := range activity.Records {
+		if record.Cadence > 100 {
+			total += record.Distance - prev
+		}
+		prev = record.Distance
+	}
+
+	// Print the sport of the first Session message
+
+	fmt.Println(total/160958, "mi above 100 rpm")
+	fmt.Println(prev/160958, "mi total")
+}
